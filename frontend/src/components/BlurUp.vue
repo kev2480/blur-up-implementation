1cc68e68b103586e27e606f831810f5ba8deada0
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-12-10 16:05:03 +0000

    Add frontend implementation

---
+++ frontend/src/components/BlurUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <BlurUpImage src="http://localhost:3000/panda1.jpg" smallSrc="http://localhost:3000/small/panda1.jpg" :height="600"></BlurUpImage>
+  </div>
+</template>
+
+<script>
+import BlurUpImage from './BlurUpImage'
+export default {
+  name: 'BlurUp',
+  components: {
+    BlurUpImage
+  }
+}
+</script>
